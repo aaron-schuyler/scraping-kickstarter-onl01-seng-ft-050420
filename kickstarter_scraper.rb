e4063257e0f33489ac6df704e5c4bafb98ecd60d
Aaron Schuyler <asky28@gmail.com>
2020-05-21 00:29:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,4 @@
-html = File.read('fixtures/kickstarter.html')
- 
-kickstarter = Nokogiri::HTML(html)
-
-
+require "nokogiri"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
